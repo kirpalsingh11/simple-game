d6345b1c33822ae0bd5f04441177247828919e45
kirpalsingh11 <kirpalsinghparmar2014@gmail.com>
2025-09-25 22:17:05 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,16 @@
 // ---------------- Game Variables ----------------
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBpDkkemB9ohWS5ZUm5T0YqPqIvxSz7Tc4",
+  authDomain: "click-game-cc39b.firebaseapp.com",
+  databaseURL: "https://click-game-cc39b-default-rtdb.firebaseio.com",
+  projectId: "click-game-cc39b",
+  storageBucket: "click-game-cc39b.firebasestorage.app",
+  messagingSenderId: "966402336573",
+  appId: "1:966402336573:web:c7e75881e384ffb8be0754",
+  measurementId: "G-LKJD88BWW3"
+};
+
 let canvas, ctx;
 let box = { x: 100, y: 100, size: 50 };
 let score = 0;
@@ -127,3 +139,4 @@ function startWithName() {
   document.getElementById("gameContainer").style.display = "block";
   startGame();
 }
+
