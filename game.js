980ae03a56fd572b547c2b73e0b3178a7d0a5491
kirpalsingh11 <kirpalsinghparmar2014@gmail.com>
2025-09-25 21:57:44 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,18 +1,3 @@
-// ---------------- Firebase Config ----------------
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBpDkkemB9ohWS5ZUm5T0YqPqIvxSz7Tc4",
-  authDomain: "click-game-cc39b.firebaseapp.com",
-  databaseURL: "https://click-game-cc39b-default-rtdb.firebaseio.com",
-  projectId: "click-game-cc39b",
-  storageBucket: "click-game-cc39b.firebasestorage.app",
-  messagingSenderId: "966402336573",
-  appId: "1:966402336573:web:c7e75881e384ffb8be0754",
-  measurementId: "G-LKJD88BWW3"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
 
 // ---------------- Global Variables ----------------
 let playerName = "";
@@ -134,4 +119,5 @@ document.getElementById("restartBtn").addEventListener("click", ()=>{
   document.getElementById("restartBtn").style.display = "none";
   startGame();
 });
+
 
